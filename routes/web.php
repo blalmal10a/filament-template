bfcc279005b5ec0b53c7215252a83cf3fb6cb824
blalmal10a <malsawma7777@gmail.com>
2025-10-16 10:29:58 +0530

    chore: add redirect login

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', LandingPage::class);
 Route::get('user-morph-one-image', [UserController::class, 'morph_one'])->name('demo.user.index');
 Route::get('images/{id}', [ImageController::class, 'getMedia']);
+
+Route::redirect('redirect-login', '/admin/login')->name('login');
